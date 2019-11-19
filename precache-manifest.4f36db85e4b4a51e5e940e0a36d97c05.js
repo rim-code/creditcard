4eb603d79275433336a48cfde338c94efc4734b9
rim-code <friouirim@gmail.com>
2019-11-19 14:16:51 +0100

    Updates

---
+++ precache-manifest.4f36db85e4b4a51e5e940e0a36d97c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2e136cba15a4c24dbb0a84d7f4699f",
+    "url": "/creditcard/index.html"
+  },
+  {
+    "revision": "57c8d8ba7602a0437832",
+    "url": "/creditcard/static/css/main.48acad90.chunk.css"
+  },
+  {
+    "revision": "784ad82ae85a9cdc4d3c",
+    "url": "/creditcard/static/js/2.3a7bd05c.chunk.js"
+  },
+  {
+    "revision": "57c8d8ba7602a0437832",
+    "url": "/creditcard/static/js/main.6f80af78.chunk.js"
+  },
+  {
+    "revision": "55c9f2bff0f48a5f90a6",
+    "url": "/creditcard/static/js/runtime-main.61e73342.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/creditcard/static/media/logo.25bf045c.svg"
+  }
+]);
